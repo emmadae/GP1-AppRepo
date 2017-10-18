c359ec9760667f6fd55dc585dfe274de94b2714e
dwc3k5 <dwc1608@gmail.com>
2017-10-18 13:15:02 -0500

    testing

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,6 @@
+
+
+
 // Document Ready
 $(document).ready(function() {
 
@@ -26,3 +29,18 @@ $(document).ready(function() {
 
 
 });
+// Initialize Firebase
+//firebase information
+var config = {
+  apiKey: "AIzaSyDRQXaMgkueFCWKSOynK2w_2e81_J7xUUM",
+  authDomain: "gp1-app.firebaseapp.com",
+  databaseURL: "https://gp1-app.firebaseio.com",
+  projectId: "gp1-app",
+  storageBucket: "gp1-app.appspot.com",
+  messagingSenderId: "922497710932"
+};
+firebase.initializeApp(config);
+
+//variables
+var database = firebase.database();
+console.log("hello World");
