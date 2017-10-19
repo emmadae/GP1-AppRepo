24d62d93119435e4a2d339f0a343393093e654c8
dwc3k5 <dwc1608@gmail.com>
2017-10-19 18:13:35 -0500

    no comment

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -98,7 +98,17 @@ console.log("running");
 	      }, 2500);
 	});
 
-
+  function initMap() {
+    var uluru = {lat: 30.286, lng: -97.731};
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    var marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
 
 
 
