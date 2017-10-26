55f6acf993c2dde13cc7ae6754659e7c549df84f
Kevin Lesane <kjl738@utexas.edu>
2017-10-26 09:29:45 -0500

    added open_now functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,7 +33,7 @@
 
     $(window).load(function() {
       $("#content-index").show();
-      $("#search-connect-btn").fadeIn(2500);
+      $("#search-connect-btn").fadeIn(2000);
     });
 
     $("#like").click(function () {
@@ -103,58 +103,35 @@
   // var baseURLGM = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
   //   keyGM + "&q=";
 
-$(document).on("click", "#reset",function(){
-  database.ref().set({
-      liked:0,
-      disliked:0
-  });
-});
-
-
-$(document).on("click", "#like", function(){
-  var good= firebase.database().ref("liked");
-  var likes;
-    good.once("value", function(snapshot){
-      likes = snapshot.val();
-      likes++;
-      database.ref().update({liked:likes});
-    });
-
-  database.ref("liked").once("value",function(snapshot){
-    $("#likes").text("Likes: "+ (parseInt(snapshot.val())+1));
-  });
-});
-// //
-// //
-// //
-$(document).on("click", "#dislike",function(){
-  var bad= firebase.database().ref("disliked");
-  var dislikes;
-    bad.once("value", function(snapshot){
-      dislikes = snapshot.val();
-      dislikes++;
-      database.ref().update({disliked:dislikes});
-    });
-
-  database.ref("disliked").on("value",function(snapshot){
-    $("#dislikes").text("Dislikes: "+ (parseInt(snapshot.val())+1));
-  });
-});
-
-
 //Users Search Parameters
 var search;
 var zipcode;
 var mAway = 1609;
-var destinationSelect = 'cafe';
+var destinationSelect = 'bar';
+var drawDistance = 14;
 //updates map using Users Search Parameters
 $(document).ready(function(){
   $("#search-btn").on("click", function(event){
     event.preventDefault();
+    $("#searchButtons").html("");
     //sets distance converting miles to meters
     miAway = parseInt($("#miles-away>option:selected").text());
     mAway = miAway*1609;
-
+    if(miAway === 1){
+      drawDistance = 14;
+    }else if(miAway === 5){
+      drawDistance = 12;
+    }else if(miAway === 10){
+      drawDistance = 11;
+    }else if(miAway === 15){
+      drawDistance = 10;
+    }else if(miAway === 20){
+      drawDistance = 10;
+    }else if(miAway === 25){
+      drawDistance = 10;
+    }else if(miAway === 30){
+      drawDistance = 10;
+    }
     //sets categories based on user specificity
     if($("#destination-select>option:selected").text()==="Coffee"){
         destinationSelect = "cafe";
@@ -179,32 +156,29 @@ $(document).ready(function(){
 
       var map;
       var infowindow;
+      var locationId= [];
+      var locationName= [];
+      var locationRating=[];
+      var locationVicinity=[];
 
       function initMap() {
         var austin = {lat: 30.286, lng: -97.731};
 
         map = new google.maps.Map(document.getElementById('map'), {
           center: austin,
-          zoom: 15
+          zoom: drawDistance
         });
 
         infowindow = new google.maps.InfoWindow();
         var service = new google.maps.places.PlacesService(map);
         service.nearbySearch({
           location: austin,
-          radius: mAway,
+          rankBy: google.maps.places.RankBy.DISTANCE,
+          // radius: mAway,
           type: [destinationSelect]
         }, callback);
       }
-
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      }
-
+// [destiantionSelect]
       function createMarker(place) {
         var placeLoc = place.geometry.location;
         var marker = new google.maps.Marker({
@@ -214,13 +188,65 @@ $(document).ready(function(){
         });
 
         google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name + "<br>" + "Rating: " + place.rating + "/5" + "<br>" + place.radius + " miles away");
+          infowindow.setContent(place.name + "<br>" + "Rating: " + place.rating + "/5" + "<br>" + "Open now: " + place.opening_hours.open_now);
           infowindow.open(map, this);
         });
       }
 
 
 initMap();
+
+
+      function callback(results, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+          for (var i = 0; i < results.length; i++){
+            createMarker(results[i]);
+            console.log(results[i]);
+            if(i<10){
+            $("#searchButtons").append("<button id="+results[i].id+" class='searchBtn'>"+results[i].name+"</button>");
+            locationId.push(results[i].id);
+            locationName.push(results[i].name);
+            locationRating.push(results[i].rating);
+            locationVicinity.push(results[i].vicinity);
+            // console.log(locationName);
+          }}}}
+
+var id;
+
+$(document).on("click", ".searchBtn", function(){
+    console.log("working");
+    id = $(this).attr("id");
+    var arrayNum = locationId.indexOf(id);
+    $("#mLocName").text(locationName[arrayNum]);
+    $("#mLocRating").text("Rating: " + locationRating[arrayNum]);
+
+    $("#mLocVicinity").text(locationVicinity[arrayNum]);
+
+    database.ref("/locations").on("value", function(snapshot){
+      if(snapshot.child(id).exists()){
+        console.log("coolio");
+      }else{
+        console.log("nope");
+        console.log(id);
+        database.ref("/locations").update({
+          //ignore syntax error on line below. working as intended
+          [id]:{
+            liked:0,
+            disliked:0
+          }
+        });
+
+      }
+      console.log(snapshot.child(id).child("liked").val());
+      var popular = snapshot.child(id).child("liked").val();
+      var unpopular = snapshot.child(id).child("disliked").val();
+      $("#userRates").text("Likes: "+popular+" | Dislikes: "+ unpopular);
+      $("#ourRate").html('<button id="mLike">LIKE <i class="fa fa-thumbs-o-up" aria-hidden="true"></i></button><p id="mLikes"> <i class="fa fa-thumbs-o-up" aria-hidden="true"></i> </p><button id="mDislike">DISLIKE <i class="fa fa-thumbs-o-down" aria-hidden="true"></i> </button><p id="mDislikes"><i class="fa fa-thumbs-o-down" aria-hidden="true"></i></p>');
+    });
+    modal.style.display = "block";
+    });
+
+
 
 
 
@@ -249,8 +275,104 @@ initMap();
   }
 
 
+//generates divs to hold information regarding best search results
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+
+// SCRIPT FOR WEBSITE CSS ANIMATION --------------------------------------------
+// Document Ready
+$(document).ready(function() {
+
+    // on click funtion for navigation bar to appear on click
+    $('#nav-icon3').click(function(){
+        $(".list-items").slideToggle(800);
+    });
+
+
+    $(window).load(function() {
+        $("#content-index").show();
+        $("#search-connect-btn").fadeIn(2500);
+    });
+
+    $(document).on("click", "#like", function () {
+      $("#likes").show(200);
+    });
+
+    $(document).on("click", "#dislike", function () {
+      $("#dislikes").show(200);
+    });
+
+});
+//modal "like" click functionality
+    $(document).on("click", "#mLike", function () {
+
+      var good= firebase.database().ref("locations/"+id);
+      var likes;
+        good.once("value", function(snapshot){
+           likes = snapshot.val().liked;
+          likes++;
+          var thePath = "locations/"+id+"/liked";
+          database.ref().update({[thePath]:likes});
+        });
+
+      database.ref("locations/"+id+"/liked").once("value",function(snapshot){
+        $("#mLikes").show(200);
+        console.log("you liked");
+        console.log(snapshot.val());
+        $("#mLikes").text("Likes: "+ (parseInt(snapshot.val())+1));
+      });
+    });
 
 
 
+//modal functionality for dislikes
+    $(document).on("click", "#mDislike", function () {
+      $("#mDislikes").show(200);
+      var bad= firebase.database().ref("locations/"+id);
+      var dislikes;
+        bad.once("value", function(snapshot){
+          dislikes = snapshot.val().disliked;
+          dislikes++;
+          var thePath = "locations/"+id+"/disliked";
+          database.ref().update({[thePath]:dislikes});
+          console.log(snapshot.val());
+        });
+        database.ref("locations/"+id+"/disliked").once("value",function(snapshot){
+            $("#mDislikes").show(200);
+          console.log("you disliked");
+          console.log(snapshot.val());
+          $("#mDislikes").text("Dislikes: "+ (parseInt(snapshot.val())+1));
+        });
+    });
+
+//this is currently not triggering properly
 
 
+
+    // overlay navigation menu
+    $("#hamburger-nav").click(function() {
+      $(".overlay-content").style.width = "100%";
+    });
+
+    $("#exit-btn").click(function() {
+      $(".overlay-content").style.width = "0%";
+    });
